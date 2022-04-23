12ae1e96bdb45a3dbf0a54e005fdb5aeeedcc72d
Daniils Petrovs <thedanpetrov@gmail.com>
2022-04-23 22:42:09 +0200

    Move video recognition funs out

--- imgproc/imgproc.go
+++ imgproc/imgproc.go
@@ -1,7 +1,6 @@
 package imgproc
 
 import (
-	"errors"
 	"fmt"
 	"image"
 	"image/color"
@@ -13,72 +12,6 @@ import (
 const OlympusMapImgPath = "./resources/maps/olympus.png"
 
 const C = 0.71 // the ratio of the size of the minimap rectange on the map to the one in the first-person view
-
-// Creates and returns a new Mat from the top left quadrant of the src Mat
-func CropTopLeftQuadrant(mat *gocv.Mat) gocv.Mat {
-	srcWidth := mat.Cols()
-	srcHeight := mat.Rows()
-
-	rect := image.Rect(0, 0, srcWidth/2, srcHeight/2)
-	cropped := mat.Region(rect)
-	return cropped
-}
-
-// Finds the minimap in a src binary image and returns a bounding Rectangle of it
-func FindMinimapRect(src *gocv.Mat) (image.Rectangle, error) {
-	const (
-		minimumArea  = 10
-		minimumAr    = 3
-		maxMinimapAr = 2
-	)
-	candidateRects := []image.Rectangle{}
-
-	contours := gocv.FindContours(*src, gocv.RetrievalExternal, gocv.ChainApproxSimple)
-	defer contours.Close()
-
-	for i := 0; i < contours.Size(); i++ {
-		contour := contours.At(i)
-
-		area := gocv.ContourArea(contour)
-		rect := gocv.BoundingRect(contour)
-
-		width := rect.Dx()
-		height := rect.Dy()
-		ar := float32(width) / float32(height)
-
-		if area > minimumArea && ar > 3 {
-			candidateRects = append(candidateRects, rect)
-		}
-	}
-
-	minimapMin := image.Point{X: 9999, Y: 9999}
-	minimapMax := image.Point{X: 0, Y: 0}
-
-	// Find extremes of the collection of rectangles part of the minimap
-	for _, candidateRect := range candidateRects {
-		if candidateRect.Min.X < minimapMin.X {
-			minimapMin.X = candidateRect.Min.X
-		}
-		if candidateRect.Min.Y < minimapMin.Y {
-			minimapMin.Y = candidateRect.Min.Y
-		}
-		if candidateRect.Max.X > minimapMax.X {
-			minimapMax.X = candidateRect.Max.X
-		}
-		if candidateRect.Max.Y > minimapMax.Y {
-			minimapMax.Y = candidateRect.Max.Y
-		}
-	}
-
-	minimapRect := image.Rect(minimapMin.X, minimapMin.Y, minimapMax.X, minimapMax.Y)
-	minimapRectAr := float32(minimapRect.Dx()) / float32(minimapRect.Dy())
-
-	if minimapRectAr < maxMinimapAr {
-		return minimapRect, nil
-	} else {
-		return image.Rectangle{}, errors.New("could not find minimap rectangle")
-	}
-}
 
 func TrackMinimapLocationFromVideoFile(filePath string, matchFrameInterval int) {
 	const frameBufferSize = 1024 // number of frames to keep in the video processing buffer
@@ -242,7 +175,9 @@ func resultsPresenter(results <-chan image.Point) {
 			break
 		}
 
-		gocv.Circle(&mapImg, result, 3, color.RGBA{0, 255, 0, 255}, 3)
+		markerColor := color.RGBA{0, 255, 0, 255}
+
+		gocv.Circle(&mapImg, result, 3, markerColor, 3)
 		window.IMShow(mapImg)
 
 		fmt.Printf("Found point at: (%d, %d)\n", result.X, result.Y)
